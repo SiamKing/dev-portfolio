b83f1490fdb0eaac0998c7a6d4936c44b4f0efaf
SiamKing <thesiameseelectric@gmail.com>
2017-12-01 13:30:43 -0800

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: 'source-greeting')
     end
   end
+
+  def copyright_generator    
+    @copyright = HillsViewTool::Renderer.copyright 'TJ Hill', 'All rights reserved'
+  end
 end

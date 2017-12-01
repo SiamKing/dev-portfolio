403e09d2df1a46991aa736cd49e0d4020bd92f97
SiamKing <thesiameseelectric@gmail.com>
2017-12-01 15:23:20 -0800

    implemented authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User)
-      link_to 'Logout', destroy_user_session_path, method: :delete
-    else
+    if current_user.is_a?(GuestUser)
       (link_to 'Sign Up', new_user_registration_path) +
       "<br>".html_safe +
       (link_to 'Login', new_user_session_path)
+    else
+      link_to 'Logout', destroy_user_session_path, method: :delete
     end
   end
 
@@ -16,7 +16,7 @@ module ApplicationHelper
     end
   end
 
-  def copyright_generator    
+  def copyright_generator
     @copyright = HillsViewTool::Renderer.copyright 'TJ Hill', 'All rights reserved'
   end
 end

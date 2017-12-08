c4c23cf8f9d41674d4d0d431e17fd4bdca0ce84a
SiamKing <thesiameseelectric@gmail.com>
2017-12-08 14:37:25 -0800

    Finished carrierwave installation

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -30,7 +30,7 @@ class PortfoliosController < ApplicationController
 
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
-    binding.pry
+    # binding.pry
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
